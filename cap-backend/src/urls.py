b837d5654ed2936b5d69332f131f1739234735ac
eklal <20029330@students.koi.edu.au>
2026-01-12 21:38:37 +1100

    refeat(user): refact login and register

--- cap-backend/src/urls.py
+++ cap-backend/src/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),
+    path('api/', include('users.urls')),  # Auth endpoints directly under /api/
     path('api/trainers/', include('trainers.urls')),
     path("api/bookings/", include("bookings.urls")),
 ]
